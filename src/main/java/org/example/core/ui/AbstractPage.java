1c38242ca2fe5678f05ddbc943f60c1f8e3a3bb2
Miguel Oscar <mixmeil@gmail.com>
2020-07-14 16:40:42 -0400

    changed the default timeout

--- src/main/java/org/example/core/ui/AbstractPage.java
+++ src/main/java/org/example/core/ui/AbstractPage.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class AbstractPage {
 
-    public static final int DEFAULT_IMPLICIT_TIMEOUT = 15;
-    public static final int EXPLICIT_WAIT_IN_SECONDS = 20;
+    public static final int DEFAULT_IMPLICIT_TIMEOUT = 20;
+    public static final int EXPLICIT_WAIT_IN_SECONDS = 30;
 
     protected WebDriver driver;
 
